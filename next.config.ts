7190a8b1fb2b6573aeb47ce7e1da384ede1f90ee
Bridger Tower <bridgertower@gmail.com>
2025-07-11 08:45:03 -0600

    add remote host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      new URL("https://du5pekqoht3fwi0h.public.blob.vercel-storage.com/**"),
+    ],
+  },
 };
 
 export default nextConfig;
